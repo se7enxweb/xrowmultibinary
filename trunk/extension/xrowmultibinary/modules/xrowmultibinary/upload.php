157e2bfc191747e46a3f99627aa51682d6d6ff34
Björn Dieding <bjoern@xrow.de>
2010-10-01 07:12:42 +0000

    fixed stream support and chunking
    git-svn-id: http://svn.projects.ez.no/xrowmultibinary@41 cf65925a-9c52-4b88-8269-47fa7225a02a

--- trunk/extension/xrowmultibinary/modules/xrowmultibinary/upload.php
+++ trunk/extension/xrowmultibinary/modules/xrowmultibinary/upload.php
@@ -69,7 +69,7 @@ $logging = false;
 $total = 0;
 if( $logging && isset( $_REQUEST['chunk'] ) )
 {
-	eZLog::write(  "CONTENT_TYPE: " .$contentType . ' Chunk: ' . $_REQUEST['chunk'] ,'upload.log');
+	eZLog::write(  "Name: ". $fileName . "CONTENT_TYPE: " .$contentType . ' Chunk ' . $_REQUEST['chunk'] . ' from ' . $_REQUEST['chunks'] , 'upload.log');
 }
 if ( strpos( $contentType, 'multipart' ) !== false )
 {
@@ -88,8 +88,10 @@ if ( strpos( $contentType, 'multipart' ) !== false )
                     fwrite( $out, $buff );
             }
             else
+            {
+                eZDebug::writeError( 'Failed to move uploaded file.', 'xrowmultibinary/upload' );
                 die( '{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}' );
-            
+            }
             fclose( $out );
             $oldumask = umask( 0 );
             chmod( $storeName, octdec( eZINI::instance()->variable( 'FileSettings', 'StorageFilePermissions' ) ) );
@@ -97,14 +99,20 @@ if ( strpos( $contentType, 'multipart' ) !== false )
             unlink( $_FILES['file']['tmp_name'] );
         }
         else
+        {
+            eZDebug::writeError( 'Failed to move uploaded file.', 'xrowmultibinary/upload' );
             die( '{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}' );
+        }
     }
     else
+    {
+        eZDebug::writeError( 'Failed to move uploaded file.', 'xrowmultibinary/upload' );
         die( '{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}' );
+    }
 }
 else
 {
-	set_time_limit(0);
+    set_time_limit(0);
     $mem = $attribute->attribute( 'contentclass_attribute' )->DataInt1 * 2;// twice the max upload size
     ini_set('memory_limit', $mem.'M');
     // Open temp file
@@ -117,26 +125,34 @@ else
         if ( $in )
         {
             while ( $buff = fread( $in, $buffer_length ) )
-			{
+	    {
                 fwrite( $out, $buff );
-				$total = filesize( $storeName );
-				if( $logging )
-				{
-					eZLog::write(  "File: " . $storeName . ' Bytes: ' . $total ,'upload.log');
-				}
-			}
+		$total = filesize( $storeName );
+		if( $logging )
+		{
+		    eZLog::write(  "File: " . $storeName . ' Bytes: ' . $total ,'upload.log');
+		}
+	    }
         }
         else
+        {
+            eZDebug::writeError( 'Failed to open input stream.', 'xrowmultibinary/upload' );
             die( '{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}' );
-        
+        }
         fclose( $out );
         $oldumask = umask( 0 );
         chmod( $storeName, octdec( eZINI::instance()->variable( 'FileSettings', 'StorageFilePermissions' ) ) );
         umask( $oldumask );
     }
     else
+    {
+        eZDebug::writeError( 'Failed to open output stream.', 'xrowmultibinary/upload' );
         die( '{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}' );
+    }
 }
+$targetchunk = $chunks -1;
+if( isset( $_REQUEST['chunk'] ) and $chunk == $targetchunk )
+{
 
 $contentObjectAttributeID = $attribute->attribute( 'id' );
 $version = $attribute->attribute( 'version' );
@@ -150,6 +166,23 @@ $binary->store();
 $fileHandler = eZClusterFileHandler::instance();
 $fileHandler->fileStore( $storeName, 'binaryfile', true, $mime );
 
+}
+elseif(!isset( $_REQUEST['chunk'] ))
+{
+
+$contentObjectAttributeID = $attribute->attribute( 'id' );
+$version = $attribute->attribute( 'version' );
+
+$binary = eZBinaryFile2::create( $contentObjectAttributeID, $version );
+$binary->setAttribute( 'filename', basename( $storeName ) );
+$binary->setAttribute( 'original_filename', $fileName );
+$binary->setAttribute( 'mime_type', $mime['name'] );
+$binary->store();
+
+$fileHandler = eZClusterFileHandler::instance();
+$fileHandler->fileStore( $storeName, 'binaryfile', true, $mime );
+
+}
 
 // Return JSON-RPC response
 echo '{"jsonrpc" : "2.0", "result" : null, "id" : "'.basename( $storeName ).'"}';
